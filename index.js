cec2d451f223b4c72b7620c98221bdcaacb06126
P <patrickmo@gmx.de>
2018-12-21 18:27:43 +0100

    try to fix es2016

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const semver = require('semver');
 const nodejsVersion = semver.parse(process.versions.node).major;
 
-const client = require(__dirname + '/lib/HarmonyHubWS');
-const clientES2016 = require(__dirname + '/lib/HarmonyHubWS_ES2016');
-
-module.exports = nodejsVersion >= 8 ? client : clientES2016;
+module.exports = nodejsVersion >= 8 ? require(__dirname + '/lib/HarmonyHubWS') : require(__dirname + '/lib/HarmonyHubWS_ES2016');
